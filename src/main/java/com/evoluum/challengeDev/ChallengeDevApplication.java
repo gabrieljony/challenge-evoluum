033b40b4bbb7273af308f38c5ffb1bde68ef9e6a
Gabriel Jony <gabjony@gmail.com>
2020-05-29 20:02:51 -0300

    first commit

---
+++ src/main/java/com/evoluum/challengeDev/ChallengeDevApplication.java
@@ -0,0 +1,13 @@
+package com.evoluum.challengeDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evoluum/challengeDev/ChallengeDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoluum.challengeDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
