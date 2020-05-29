b6ff6c1278e1c21d419c571b68aed8e7d43d76cb
Gabriel Jony <gabjony@gmail.com>
2020-05-29 20:42:49 -0300

    Configuração do RestTemplate

--- src/main/java/com/evoluum/challengeDev/ChallengeDevApplication.java
+++ src/main/java/com/evoluum/challengeDev/ChallengeDevApplication.java
@@ -2,6 +2,10 @@ package com.evoluum.challengeDev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ChallengeDevApplication {
@@ -10,4 +14,8 @@ public class ChallengeDevApplication {
 		SpringApplication.run(ChallengeDevApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate rest(RestTemplateBuilder builder) {
+		return  new RestTemplate( new HttpComponentsClientHttpRequestFactory() );
+	}
 }
